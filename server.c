a57f5e715c23f9456d9ca3719cd619899dc3fddd
milannedic <milan.nedic94@gmail.com>
2019-11-24 19:17:37 +0000

    first commit

--- server.c
+++ server.c
@@ -1,16 +1,73 @@
 #include <stdio.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <dirent.h>
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
-typedef enum { IDLE, STRING, LS, FILE_SIZE, SENDING } ftp_state;
+#include <unistd.h>
+#include <sys/mman.h>
+#include <fcntl.h>
 
+#define MAX_PKT_SIZE (640*480*4)
 
-ftp_state stat = IDLE; 
+#define BLUE 0x001f
+#define BLACK 0x0000
+#define YELLOW 0xffe0
+#define RED 0xf800
+#define GREEN 0x07e0
+
+unsigned int vectorColor[480][640] = {{0}};
+
+void updateScreen(void)
+{
+	FILE *fp;
+	int fd, *p;
+	fd = open("/dev/vga_dma", O_RDWR|O_NDELAY);
+	if( fd < 0)
+	{
+		printf("Cannot opet /dev/vga for write\n");
+		return;
+	}
+	p = (int*)mmap(0,640*480*4, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	memcpy(p, vectorColor, MAX_PKT_SIZE);
+	munmap(p, MAX_PKT_SIZE);
+	close(fd);
+	if(fd < 0)
+		printf("Cannot close /dev/vga for write\n");
+}
+
+void fillVector(const int color, const int Xmin, const int Xmax, const int Ymin, const int Ymax)
+{
+	int x,y;
+	for(x=Xmin; x<Xmax; ++x)
+		for(y=Ymin; y<Ymax; ++y)
+			vectorColor[y][x] = color;
+}
+
+void fillHorizLine(const int color, const int Xmin, const int Xmax, const int Y)
+{
+	fillVector(color, Xmin, Xmax, Y, Y+1);
+	updateScreen();	
+}
+
+void fillVertLine(const int color, const int X, const int Ymin, const int Ymax)
+{
+	fillVector(color, X, X+1, Ymin, Ymax);
+	updateScreen();
+}
+
+void fillRectangle(const int color, const int X0, const int X1, const int Y0, const int Y1)
+{
+	int i;
+	for(i=Y0; i < Y1 ; ++i)
+		fillHorizLine(color, X0, X1, i);
+}
+
 int selection = 0;
 int filesize;
 int sent = 0;
@@ -44,6 +101,11 @@ void doprocessing (int sock)
 
 int main( int argc, char **argv )
 {
+
+	 fillRectangle(RED, 10,50,10,50);
+	 fillHorizLine(BLUE, 0, 640, 240);
+	 fillVertLine(BLUE, 320,0,480);
+
     int sockfd, newsockfd, portno, clilen;
     char buffer[256];
     struct sockaddr_in serv_addr, cli_addr;
@@ -74,10 +136,6 @@ int main( int argc, char **argv )
 			{
 				printf("FTP client connected...\n");
 				++incr;
-			}
-			else if(incr == 4)
-			{
-				printf("too many clients\n");
 			}
 			   
         	if (newsockfd < 0)
